448ac4f6a2e0e298a3e6f96bf2196e080f7c302a
Richard Vaughan <richard.vaughan@xuvasi.com>
2017-07-06 19:33:51 +0100

    Fiddling.

--- 2017-07-grpc/team1/cmd/client/main.go
+++ 2017-07-grpc/team1/cmd/client/main.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+    "flag"
+    "log"
+
+    "../../pb/say"
+
+    "google.golang.org/grpc"
+)
+
 
 func main() {
-    
+    backend := flag.String("backend", "127.0.0.1:8888", "Where is the backend?")
+    message := flag.String("message", "Hello World", "Some message to say")
+
+    flags.Parse()
+
+    conn, err := grpc.Dial(*backend, grpc.WithInsecure())
+    if (err != nil) {
+        log.Panic(err)
+    }
+
+
+
+    defer conn.Close()
 }
