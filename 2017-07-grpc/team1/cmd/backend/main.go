e1e90e7a0ebb261170e246ed0d15ababf19bb28c
Eleanor Deal <eleanor.deal@gmail.com>
2017-07-06 20:37:13 +0100

    Use tmp files for storing command output

--- 2017-07-grpc/team1/cmd/backend/main.go
+++ 2017-07-grpc/team1/cmd/backend/main.go
@@ -4,7 +4,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
 	"os/exec"
+	"strconv"
 
 	pb "github.com/golang-cymru/cardiff-meetup-code/2017-07-grpc/team1/pb/say"
 	"golang.org/x/net/context"
@@ -16,23 +18,32 @@ const (
 	port = ":8888"
 )
 
-// server is the server
 type server struct{}
 
 // SaySomething does what it says
 func (s *server) SaySomething(ctx context.Context, in *pb.Something) (*pb.Result, error) {
 	log.Println("Saying something: " + in.Message)
-	cmd := exec.Command("say", in.Message, "-o", "said.aiff")
-	err := cmd.Run()
+
+	file, err := ioutil.TempFile(".", "said")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	b, err := ioutil.ReadFile("said.aiff")
+	file.Close()
+	defer os.Remove(file.Name())
+
+	cmd := exec.Command("say", in.Message, "-o", file.Name())
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(file.Name() + ".aiff")
+
+	b, err := ioutil.ReadFile(file.Name() + ".aiff")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	log.Println("Results being sent: " + strconv.Itoa(len(b)))
 	return &pb.Result{Audio: b}, nil
 }
 
@@ -43,6 +54,7 @@ func main() {
 	}
 	s := grpc.NewServer()
 	pb.RegisterTextToSpeechServer(s, &server{})
+
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
